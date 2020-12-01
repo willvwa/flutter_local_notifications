6891bdf7bba742454b80b818f167c77af21e0557
willvwa <willvwa@gmail.com>
2020-12-01 15:09:54 -0300

    Teste de comunicação flutter / Android platform 2

--- flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/models/NotificationDetails.java
+++ flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/models/NotificationDetails.java
@@ -3,6 +3,7 @@ package com.dexterous.flutterlocalnotifications.models;
 import android.graphics.Color;
 import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
+import android.util.Log;
 
 import androidx.annotation.Keep;
 
@@ -214,7 +215,7 @@ public class NotificationDetails {
         }
         if (arguments.containsKey(ACTIONS)) {
 
-            System.out.println(arguments.get(ACTIONS).toString());
+            Log.d("ANDROID RESULTADO", arguments.get(ACTIONS).toString());
 
 //            List<Object> notParsedActions = (ArrayList<Object>) arguments.get(ACTIONS);
 //
